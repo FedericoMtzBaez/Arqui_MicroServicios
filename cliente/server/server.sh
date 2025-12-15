54809df1b098a8a1cc5a51bac6a3e8b10fdf192e
Fede <37182635@uaz.edu.mx>
2025-12-15 00:41:12 -0600

    reestructura del repositorio

--- cliente/server/server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=80
-
-fuser -k 80/tcp
-
-cd build/web
-
-python3 -m http.server $PORT
--- gestor-de-clientes/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from mongomock import MongoClient
-
-class PyMongoMock(MongoClient):
-    def init_app(self, app):
-        return super().__init__()
